8106aeabbd1f37c1dabb08237c235e3018c5dced
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-08 14:12:17 +0430

    changed profile group to users

--- routes/profile.php
+++ routes/profile.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Profile\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 //actions that need authentication
-Route::middleware('auth:sanctum')->group(function(){
-    Route::prefix('profiles')->name('profiles.')->group(function(){
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('users')->name('users.')->group(function () {
         //update the authenticated user's profile
         Route::patch('', [ProfileController::class, 'update'])->name('update');
     });
