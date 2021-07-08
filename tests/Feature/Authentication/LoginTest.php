2f9a813c0bd014ebad12817b8ba9c6e84a7a970a
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-08 12:34:45 +0430

    added test for login feature

---
+++ tests/Feature/Authentication/LoginTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Authentication;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function users_can_login()
+    {
+        $user = User::factory()->create(['password' => Hash::make($password = $this->faker->password())]);
+
+        $this->postJson(
+            route('login'),
+            ['email' => $user->email, 'password' => $password]
+        )->assertOk()->assertJsonStructure(['data' => ['token']]);
+
+        $this->postJson(
+            route('login'),
+            ['email' => $user->email, 'password' => Str::random()]
+        )->assertUnauthorized();
+    }
+}
