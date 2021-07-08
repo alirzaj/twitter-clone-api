64d5d3d343f8a5c893d855ed1ecefe2c194e5a1d
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-08 11:02:22 +0430

    made username & phone columns unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 70);
-            $table->string('username', 100);
+            $table->string('username', 100)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone')->nullable();
+            $table->string('phone')->nullable()->unique();
             $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
             $table->tinyText('bio')->nullable();
