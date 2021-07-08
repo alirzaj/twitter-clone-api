985e826b4a0c779fc296c94abd01e823e80ece33
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-08 12:14:45 +0430

    implemented UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,14 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => $this->faker->phoneNumber(),
+            'phone_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'bio' => $this->faker->text(200),
+            'location' => $this->faker->text(100),
             'remember_token' => Str::random(10),
         ];
     }
