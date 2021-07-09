70c3126be47883b72cfaa28f1a3035e441cda9e1
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-09 18:12:02 +0430

    added retweets table migration

---
+++ database/migrations/2021_07_09_180649_create_retweets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRetweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('retweets', function (Blueprint $table) {
+            $table->foreignId('tweet_id')
+                ->constrained('tweets')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('retweets');
+    }
+}
