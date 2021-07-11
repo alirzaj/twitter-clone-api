639b974582a6d5a9512d5228a07010b65b455247
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-11 20:36:33 +0430

    added reply & parent relations

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tweet extends Model
 {
@@ -17,8 +18,18 @@ class Tweet extends Model
      */
     protected $guarded = ['id', 'likes', 'impressions', 'retweets'];
 
-    public function user() : BelongsTo
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class,'user_id','id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function reply(): HasMany
+    {
+        return $this->hasMany(Tweet::class, 'parent_tweet_id', 'id');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Tweet::class, 'parent_tweet_id', 'id');
     }
 }
