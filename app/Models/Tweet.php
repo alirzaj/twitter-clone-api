676232eecd007c8dd695a0fc2b5a0cbf339b413b
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-13 19:36:48 +0430

    added replies to guarded & renamed impressions to impressions_count

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tweet extends Model
@@ -16,7 +17,7 @@ class Tweet extends Model
      *
      * @var string[]|bool
      */
-    protected $guarded = ['id', 'likes', 'impressions', 'retweets'];
+    protected $guarded = ['id', 'likes', 'impressions_count', 'retweets', 'replies'];
 
     public function user(): BelongsTo
     {
