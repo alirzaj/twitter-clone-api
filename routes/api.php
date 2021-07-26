d8b96fd3f4481d42ec11df0dd7f9c5e9fa60108d
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-07-26 20:01:24 +0430

    added home (feed) route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Feed\HomeController;
+use Illuminate\Support\Facades\Route;
+
 require 'authentication.php';
 require 'user.php';
 require 'tweet.php';
+
+Route::middleware('auth:sanctum')->group(function () {
+    //see the authenticated user's home (feed)
+    Route::get('home', [HomeController::class, 'index'])->name('home.index');
+});
