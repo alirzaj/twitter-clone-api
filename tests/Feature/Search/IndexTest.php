1517503c540c03afbb688b0b2fcb2cd955599a69
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-08-18 19:32:31 +0430

    added 2 tests to assert indexing replies in elasticsearch

--- tests/Feature/Search/IndexTest.php
+++ tests/Feature/Search/IndexTest.php
@@ -40,3 +40,46 @@ test('when users tweet sth it will be indexed in elasticsearch via queue', funct
 
     Queue::assertPushedOn(config('elasticsearch.queue'), IndexDocument::class);
 });
+
+test('when users reply a tweet it will be indexed in elasticsearch', function () {
+    $user = User::factory()->create();
+    $tweet = Tweet::factory()->create();
+
+    Sanctum::actingAs($user);
+
+    $this->postJson(
+        route('tweets.replies.store', ['tweet' => $tweet->id]),
+        ['text' => $text = $this->faker->text(60_000)]
+    )->assertCreated();
+
+    $reply = Tweet::query()
+        ->latest('id')
+        ->where('parent_tweet_id', $tweet->id)
+        ->first()
+        ->toArray();
+
+    $this->assertElasticsearchHas('tweets', [
+        'id' => $reply['id'],
+        'text' => $reply['text'],
+        'created_at' => $reply['created_at'],
+        'parent_tweet_id' => $reply['parent_tweet_id'],
+        'user_id' => $user['id'],
+        'user_ip' => '127.0.0.1'
+    ]);
+});
+
+test('when users reply a tweet it will be indexed in elasticsearch via queue', function () {
+    $user = User::factory()->create();
+    $tweet = Tweet::factory()->create();
+
+    Sanctum::actingAs($user);
+
+    Queue::fake();
+
+    $this->postJson(
+        route('tweets.replies.store', ['tweet' => $tweet->id]),
+        ['text' => $text = $this->faker->text(60_000)]
+    )->assertCreated();
+
+    Queue::assertPushedOn(config('elasticsearch.queue'), IndexDocument::class);
+});
