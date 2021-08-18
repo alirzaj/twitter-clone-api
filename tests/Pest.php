b0c3b1971c022fe1ed25c69b6ba3fab43a30111e
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-08-18 19:09:26 +0430

    used withFaker & RefreshDatabase in every test as a trait

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Artisan;
 
 uses(Tests\TestCase::class)->in('Feature', 'Unit');
+uses(RefreshDatabase::class)->in('Feature');
+uses(WithFaker::class)->in('Feature');
 
 uses()->beforeEach(fn() => Artisan::call('elastic:create-index'))->in('Feature');
 uses()->afterEach(fn() => Artisan::call('elastic:delete-index'))->in('Feature');
