bd65296430d5d0889f9cf9f04c5259f777325768
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-08-18 19:16:19 +0430

    added toArray functionality

--- app/Elasticsearch/Indexes/Tweets.php
+++ app/Elasticsearch/Indexes/Tweets.php
@@ -2,6 +2,8 @@
 
 namespace App\Elasticsearch\Indexes;
 
+use Illuminate\Database\Eloquent\Model;
+
 class Tweets
 {
     /**
@@ -27,4 +29,26 @@ class Tweets
         'retweets_count' => 'unsigned_long',
         'replies_count' => 'unsigned_long',
     ];
+
+    /**
+     * return an array of model ready to be indexed
+     *
+     * @param Model $model
+     * @return array
+     */
+    public function toArray(Model $model): array
+    {
+       return array_filter([
+           'id' => $model->id,
+           'text' => $model->text,
+           'user_id' => $model->user_id,
+           'parent_tweet_id' => $model->parent_tweet_id,
+           'user_ip' => request()->ip(),
+           'created_at' => $model->created_at,
+           'likes_count' => $model->likes_count,
+           'impressions_count' => $model->impressions_count,
+           'retweets_count' => $model->retweets_count,
+           'replies_count' => $model->replies_count,
+       ]);
+    }
 }
