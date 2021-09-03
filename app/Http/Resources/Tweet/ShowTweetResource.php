f868afb7e1dd2c6c12e0a4669002c9b3f20a87ff
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-09-03 14:54:01 +0430

    removed useless updated_at & fixed likes_count column call

--- app/Http/Resources/Tweet/ShowTweetResource.php
+++ app/Http/Resources/Tweet/ShowTweetResource.php
@@ -17,15 +17,14 @@ class ShowTweetResource extends JsonResource
         return [
             'id' => $this->id,
             'text' => $this->text,
-            'likes' => $this->likes,
+            'likes_count' => $this->likes_count ?? 0,
             'impressions_count' => $this->impressions_count,
-            'retweets_count' => $this->retweets_count,
-            'replies_count' => $this->replies_count,
+            'retweets_count' => $this->retweets_count ?? 0,
+            'replies_count' => $this->replies_count ?? 0,
             'user' => $this->whenLoaded('user', function () {
                 return new ShowTweetUserResource($this->user);
             }),
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
         ];
     }
 }
