a356818bde1ba4c4986e0e7a5bcbb0071a588588
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-09-09 10:28:17 +0430

    added analyzer & tokenizer to handle hashtags

--- app/Elasticsearch/Indexes/Tweets.php
+++ app/Elasticsearch/Indexes/Tweets.php
@@ -26,6 +26,41 @@ class Tweets
         'created_at' => 'date',
     ];
 
+    public array $fields = [
+        'text' => [
+            'hashtags' => [
+                'type' => 'text',
+                'analyzer' => 'hashtag'
+            ]
+        ]
+    ];
+
+    /**
+     * define and config analyzers for index
+     *
+     * @var array|\string[][]
+     */
+    public array $analyzers = [
+        'hashtag' => [
+            'type' => 'custom',
+            'tokenizer' => 'hashtag_tokenizer',
+            'filter' => ['lowercase']
+        ]
+    ];
+
+    /**
+     * define & config tokenizers for index
+     *
+     * @var array
+     */
+    public array $tokenizers = [
+        'hashtag_tokenizer' => [
+            'type' => 'pattern',
+            'pattern' => '#\S+',
+            'group' => 0
+        ]
+    ];
+
     /**
      * return an array of model ready to be indexed
      *
@@ -34,16 +69,16 @@ class Tweets
      */
     public function toArray(Model $model): array
     {
-       return array_filter([
-           'id' => $model->id,
-           'text' => $model->text,
-           'user_id' => $model->user_id,
-           'parent_tweet_id' => $model->parent_tweet_id,
-           'user_ip' => request()->ip(),
-           'likes_count' => $model->likes_count,
-           'retweets_count' => $model->retweets_count,
-           'replies_count' =>$model->replies_count,
-           'created_at' => $model->created_at,
-       ]);
+        return array_filter([
+            'id' => $model->id,
+            'text' => $model->text,
+            'user_id' => $model->user_id,
+            'parent_tweet_id' => $model->parent_tweet_id,
+            'user_ip' => request()->ip(),
+            'likes_count' => $model->likes_count,
+            'retweets_count' => $model->retweets_count,
+            'replies_count' => $model->replies_count,
+            'created_at' => $model->created_at,
+        ]);
     }
 }
